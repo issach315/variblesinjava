19db9dc6d0992e5a0975ff3259926005fab72ba9
issach315 <issachemani227@gmial.com>
2023-01-10 14:24:30 +0530

    first commit

---
+++ Day01.java
@@ -0,0 +1,26 @@
+package Varibale_folder;
+
+public class Day01 
+{
+   
+    public static void main(String []args)
+    {
+        int x=10;
+        char ch='I';
+        double d=8.7;
+        String s="issach";
+        long phno=12345678910L;
+        boolean b=true;
+        System.out.println(x);
+        System.out.println(ch);
+        System.out.println(d);
+        System.out.println(s);
+        System.out.println(phno);
+        System.out.println(b);
+
+    }
+    
+}
+
+    
+
